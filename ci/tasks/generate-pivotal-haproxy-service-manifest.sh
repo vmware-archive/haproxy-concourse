97deaa29fa8c747016e576495ebbb42f3a4f6eba
John Calabrese <jcalabrese@pivotal.io>
2016-12-06 14:52:45 -0500

    [#135176031] bootstrapping haproxy-concourse pipeline layout

---
+++ ci/tasks/generate-pivotal-haproxy-service-manifest.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+chmod +x omg-cli/omg-linux
+
+omg-cli/omg-linux register-plugin \
+  -type product \
+  -pluginpath haproxy-plugin/$PRODUCT_PLUGIN
+
+omg-cli/omg-linux deploy-product \
+  --bosh-url $BOSH_URL \
+  --bosh-port $BOSH_PORT \
+  --bosh-user $BOSH_CLIENT \
+  --bosh-pass $BOSH_CLIENT_SECRET \
+  --print-manifest \
+  --ssl-ignore \
+  $PRODUCT_PLUGIN \
+  --deployment-name $DEPLOYMENT_NAME \
+  --stemcell-ver $STEMCELL_VERSION > manifest/deployment.yml
+
+#eof
