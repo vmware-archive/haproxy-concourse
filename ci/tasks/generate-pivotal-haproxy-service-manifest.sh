4f1492b9f481b56b77b29c38b78228e09f5e2f47
John Calabrese <jcalabrese@pivotal.io>
2016-12-07 11:31:04 -0500

    [#135176031] manifest generation script now leverages given list of certs

--- ci/tasks/generate-pivotal-haproxy-service-manifest.sh
+++ ci/tasks/generate-pivotal-haproxy-service-manifest.sh
@@ -1,11 +1,35 @@
-#!/bin/bash -e
+#!/bin/bash
 
+CERTFILE=certblob.cert
+CERTS_PREFIX=haproxy-cert-
+
+echo "setting permissions on omg-cli..."
 chmod +x omg-cli/omg-linux
+if [[ $? -ne 0 ]]; then 
+  cleanfail "couldnt chmod omg-cli"
+fi
 
+echo "breaking up cert blob into individual files..."
+echo "$HAPROXY_CERTS_BLOB" > ${CERTFILE}
+csplit -f ${CERTS_PREFIX} ${CERTFILE} '/^-----BEGIN CERTIFICATE-----/' '{1}' \
+  && export OMG_CERT_FILEPATH=$(ls -mx ${CERTS_PREFIX}* | sed 's/ //g') \
+  && rm -f ${CERTFILE}
+
+if [[ $? -ne 0 ]]; then 
+  cleanfail "couldnt create your certs from blob"
+fi
+echo "using certs from files: $OMG_CERT_FILEPATH" 
+echo "registering plugin..."
 omg-cli/omg-linux register-plugin \
   -type product \
   -pluginpath haproxy-plugin/$PRODUCT_PLUGIN
 
+
+if [[ $? -ne 0 ]]; then 
+  cleanfail "couldnt register your plugin"
+fi
+
+echo "generating deployment manifest..."
 omg-cli/omg-linux deploy-product \
   --bosh-url $BOSH_URL \
   --bosh-port $BOSH_PORT \
@@ -17,4 +41,22 @@ omg-cli/omg-linux deploy-product \
   --deployment-name $DEPLOYMENT_NAME \
   --stemcell-ver $STEMCELL_VERSION > manifest/deployment.yml
 
+if [[ $? -ne 0 ]]; then 
+  cleanfail "couldnt generate your manifest"
+fi
+echo "completed successfully"
+clean
+exit 0
+
+function cleanfail {
+  echo "FAILURE!!!"
+  echo $1
+  clean
+  exit 1
+} 
+
+function clean {
+  rm -f ${CERTFILE}
+  rm -f ${CERTS_PREFIX}* 
+}
 #eof
